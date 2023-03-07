d19b67e1d59a25ea8258a5ba588c21cdbaf1e1e9
Maciej Muter <maciejmuter@gmail.com>
2023-03-07 21:51:13 +0100

    first files

---
+++ wordly/src/components/Board.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import SingleLetter from "./SingleLetter";
+
+function Board() {
+  return (
+    <div className="board">
+      <div className="board__row">
+        <SingleLetter letterPos={0} attemptVal={0} />
+        <SingleLetter letterPos={1} attemptVal={0} />
+        <SingleLetter letterPos={2} attemptVal={0} />
+        <SingleLetter letterPos={3} attemptVal={0} />
+        <SingleLetter letterPos={4} attemptVal={0} />
+      </div>
+      <div className="board__row">
+        <SingleLetter letterPos={0} attemptVal={1} />
+        <SingleLetter letterPos={1} attemptVal={1} />
+        <SingleLetter letterPos={2} attemptVal={1} />
+        <SingleLetter letterPos={3} attemptVal={1} />
+        <SingleLetter letterPos={4} attemptVal={1} />
+      </div>
+      <div className="board__row">
+        <SingleLetter letterPos={0} attemptVal={2} />
+        <SingleLetter letterPos={1} attemptVal={2} />
+        <SingleLetter letterPos={2} attemptVal={2} />
+        <SingleLetter letterPos={3} attemptVal={2} />
+        <SingleLetter letterPos={4} attemptVal={2} />
+      </div>
+      <div className="board__row">
+        <SingleLetter letterPos={0} attemptVal={3} />
+        <SingleLetter letterPos={1} attemptVal={3} />
+        <SingleLetter letterPos={2} attemptVal={3} />
+        <SingleLetter letterPos={3} attemptVal={3} />
+        <SingleLetter letterPos={4} attemptVal={3} />
+      </div>
+      <div className="board__row">
+        <SingleLetter letterPos={0} attemptVal={4} />
+        <SingleLetter letterPos={1} attemptVal={4} />
+        <SingleLetter letterPos={2} attemptVal={4} />
+        <SingleLetter letterPos={3} attemptVal={4} />
+        <SingleLetter letterPos={4} attemptVal={4} />
+      </div>
+      <div className="board__row">
+        <SingleLetter letterPos={0} attemptVal={5} />
+        <SingleLetter letterPos={1} attemptVal={5} />
+        <SingleLetter letterPos={2} attemptVal={5} />
+        <SingleLetter letterPos={3} attemptVal={5} />
+        <SingleLetter letterPos={4} attemptVal={5} />
+      </div>
+    </div>
+  );
+}
+
+export default Board;
---
+++ wordly/src/components/SingleLetter.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function SingleLetter({
+  letterPos,
+  attemptVal,
+}: {
+  letterPos: number;
+  attemptVal: number;
+}) {
+  return <div className="letter">Letter</div>;
+}
+
+export default SingleLetter;
