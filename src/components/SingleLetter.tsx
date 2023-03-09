b7ba4075ec9819167e1b74cf2201c8fd15dbe90f
Maciej Muter <maciejmuter@gmail.com>
2023-03-10 00:27:33 +0100

    fix checking letters

--- src/components/SingleLetter.tsx
+++ src/components/SingleLetter.tsx
@@ -8,7 +8,8 @@ const SingleLetter: FC<SingleLetterProps> = ({ attemptVal, letterPos }) => {
   const letter = board[attemptVal][letterPos];
 
   const correct = correctWord[letterPos] === letter;
-  const almost = !correct && letter !== "" && correctWord.includes(letter);
+  const almost =
+    letter !== correct && letter !== "" && correctWord.includes(letter);
 
   const letterState =
     currAttempt.attempt > attemptVal
